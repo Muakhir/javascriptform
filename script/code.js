dc97a351e70aa8bde30fb850f0d4b7729e979d89
Muakhir <almuakhirjones@gmail.com>
2023-11-09 09:48:48 +0200

    1

--- script/code.js
+++ script/code.js
@@ -1,3 +1,13 @@
-let numb1 = 
-let numb2 = 
-let [numb1 + numb2] = true
+//addition button
+const btnadd = 
+document.querySelector('[data-submit]')
+const lboutput = document.querySelector('#output')
+
+function addition(){ 
+    let numb1 = document.getElementById('numb1').value
+    let numb2 = document.getElementById('numb2').value
+    let sum = +numb1 + +numb2
+    lboutput.textContent = sum
+
+}
+    btnadd.addEventListener('click', addition)
