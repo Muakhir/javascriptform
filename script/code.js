ecc7647a3f181bcb765631bd262513958918c805
Muakhir <almuakhirjones@gmail.com>
2023-11-20 14:59:23 +0200

    m&s

--- script/code.js
+++ script/code.js
@@ -107,7 +107,7 @@
 //     cnt++;
 // }
 
-let numbers = [12,18,12,11,5,18,2,8,2];
+// let numbers = [12,18,12,11,5,18,2,8,2];
 // for(let i =0; i<numbers.length; i++){
 //     console.log
 //     (`${i} => ${numbers[i]}`);
@@ -184,31 +184,164 @@ let greeting = " Hello World "
 console.log(greeting.trim().split(``))
 
 // Literal object
-console.log("Object literal");
-let person = {
-    firstName: 'Joel',
-    lastName: 'Mukanya'
+// console.log("Object literal");
+// let person = {
+//     firstName: 'Joel',
+//     lastName: 'Mukanya'
+// }
+// console.log(person);
+// console.log("Object.create()");
+// let smartPhone = Object.create(
+//     {
+//         make: "Samsung",
+//         model: "A 51"
+//     }
+// )
+// console.log(smartPhone);
+// console.log("new keyword | Object constructor");
+// let computer = new Object({
+//     brand: "Dell",
+//     amount: 18000
+// })
+// console.log(computer);
+
+// Factory function
+/* All variables and functions names must be in camelCase, except for the constructor function, and class names will be PascalCase or UpperCamelCase.
+======
+A factory function allow us to create and return an object.
+*/
+// function person(firstName, lastName, age, hrs, rate) {
+//   return {
+//       firstName, lastName, age, hrs, rate
+//   }
+// }
+// let person1 = person('Peter', 'Mukanya', 3, 60, 500)
+// let person2 = person('John', 'Mukanya', 1, 20, 350)
+// console.log(person1);
+// console.log(person2);
+
+// dynamic nature of an object
+
+
+
+// functions objects
+// let person ={
+//   firstName :'John',
+//   lastName:'Bruce',
+//   fullName:function(){
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// console.log(person);
+// console.log(person.fullName());
+
+// let person ={
+//   firstName :'John',
+//   lastName:'Bruce',
+//   hrs: 120,
+//   rate: 500,
+//   salary:function(){
+//     return this.hrs * this.rate
+//   }
+// }
+// console.log(person.salary())
+// person.salary()
+
+// // looping through objects
+
+// let laptop ={
+//   make:'Dell',
+//   cpu:'3ghz',
+//   ram: '16gb'
+// }
+// console.log(laptop);
+// for(let keyName in laptop){
+//   console.log(laptop[keyName]);
+// }
+// console.log(Object.keys(laptop))
+// console.log(Object.values(laptop))
+
+// Object.keys(laptop).forEach((key)=>{
+//   console.log(`${key} -> ${laptop[key]}`)
+// })
+// Object.values(laptop).forEach((value)=>{
+//   console.log(`${value}`)
+// })
+
+
+// cloning an objects using (spread op, object.assign(),jsoon.parse(),)
+
+let Car1 ={
+  make: 'Toyota',
+  year: 2023
 }
-console.log(person);
-console.log("Object.create()");
-let smartPhone = Object.create(
-    {
-        make: "Samsung",
-        model: "A 51"
-    }
-)
-console.log(smartPhone);
-console.log("new keyword | Object constructor");
-let computer = new Object({
-    brand: "Dell",
-    amount: 18000
+// let Car2 = Car1
+// console.log("==car1==");
+// console.log(Car1);
+// console.log("==car2==");
+// console.log(Car2);
+// console.log("change the value of car2");
+// Car2.year = 2024
+// console.log(Car1,Car2);
+
+// spread op
+
+let Car2 = {...Car1}
+console.log(Car1,Car2);
+console.log("change the value of car2");
+Car2.year = 2024
+console.log(Car1,Car2);
+
+//object.assign() 
+
+let car3 =Object.assign({},Car1)
+console.log("===car3===");
+console.log(car3);
+
+// json.parse()
+
+let car4 = JSON.parse(JSON.stringify(Car2))
+console.log("===car4===");
+console.log(car4);
+
+
+
+// math methods
+//  let rIdx = Math.floor(math.random() * (numbers.length - 1))
+//  console.log(numbers)
+//  console.log(rIdx)
+//  crossOriginIsolated.log(numbers[rIdx])
+
+// string methods
+
+// - charAt()
+ let name = "muakhir"
+  console.log(name.charAt(3))
+// - endsWith()
+  let text = "i come from a place far away"
+  console.log(text.endsWith("ay"));
+// - includes()
+let tex = "my name is muakhir jones"
+console.log(tex.includes(name))
+// - lastIndexOf()
+// - indexOf()
+// - replace()
+// - repeat()
+// - startWith()
+// - subString()
+// - trim()
+// - trimEnd()
+// - trimStart()
+// - slice()
+// - split()
+
+
+// map and reduce
+let numbers = [2,4,8,11,1]
+let modify = numbers.map( (x)=>{
+  return x*2
 })
-console.log(computer);
-
-
-
-
-
-
-
-
+console.log(modify)
+// reduce
+let sum =numbers.reduce((a,b)=> a+b)
+console.log(sum)
